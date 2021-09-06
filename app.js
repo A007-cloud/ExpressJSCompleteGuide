65079199e7d97e2fd49ba153098e10c98efea723
A007-cloud <dasaditya788@gmail.com>
2021-09-06 11:24:24 +0530

    init

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // console.log(routes.someText);
 
 const app = express();
 
- app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
  });
 
- app.use((req, res, next) => {
-    console.log('In another middleware!');
+ app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+ });
+
+ app.use('/', (req, res, next) => {
     res.send('<h1>Hello form Express!</h1>');
- })
+ });
+
+
 
 app.listen(3000);
 
